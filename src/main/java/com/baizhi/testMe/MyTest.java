c2dfa9fb377d906a1a7d1b37a97980e20e8563a3
ddchengit <16619970752@163.com>
2018-07-02 14:17:03 +0800

    添加了一句对话

--- src/main/java/com/baizhi/testMe/MyTest.java
+++ src/main/java/com/baizhi/testMe/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("你好");
+        System.out.println("who are you?");
     }
 }
